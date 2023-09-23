44d41c515b5121edeea3fb642a04eb29f34c7a92
MishkaRogachev <mishkarogachev@gmail.com>
2023-09-23 22:50:45 +0300

    (Server): Add idle REST server with warp and tokio

---
+++ server/src/lib.rs
@@ -0,0 +1,13 @@
+use warp::Filter;
+
+pub async fn serve() {
+    println!("Starting Dreka server");
+
+    // GET /hello/warp => 200 OK with body "Hello, warp!"
+    let hello = warp::path!("hello" / String)
+        .map(|name| format!("Hello, {}!", name));
+
+    warp::serve(hello)
+        .run(([127, 0, 0, 1], 45486))
+        .await;
+}
---
+++ server/src/main.rs
@@ -0,0 +1,6 @@
+
+
+#[tokio::main]
+async fn main() {
+    dreka_server::serve().await;
+}
