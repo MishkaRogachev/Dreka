da52367b20a494ce8dfc80d53757887384b855c3
MishkaRogachev <mishkarogachev@gmail.com>
2023-09-24 17:43:43 +0300

    (Client): Idle tauri client added

---
+++ client/src/App.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import Ping from './lib/Ping.svelte'
+</script>
+
+<main class="container">
+  <h1>Welcome to Dreka!</h1>
+
+  <div class="row">
+    <Ping />
+  </div>
+
+</main>
---
+++ client/src/lib/Ping.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  let pongMsg = ""
+
+  async function greet() {
+    // TODO: ping server to get response
+  }
+</script>
+
+<div>
+  <form class="row" on:submit|preventDefault={greet}>
+    <button type="submit">Ping</button>
+  </form>
+  <p>{pongMsg}</p>
+</div>
